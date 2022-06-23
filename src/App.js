64416f9cdb98d4bdff608307c9d56029758aa479
nikolagk <nikolag@umich.edu>
2022-06-23 02:16:09 -0500

    Took out skills

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 import Header from "./Components/Header";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   constructor(props) {
@@ -44,7 +43,6 @@ class App extends Component {
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
       </div>
     );
   }
